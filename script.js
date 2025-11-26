27541c6f3c9a406f4087d80c89e1d14750b19c22
um-shuhaib <shuhaib5560@gmail.com>
2025-11-26 13:04:46 +0530

    day 1

--- script.js
+++ script.js
@@ -1,4 +1,154 @@
-console.log("external script");
-function sum(){
-    console.log(2)
-}
+// console.log("external script");
+
+
+
+// data types in js 
+
+// string , number, booleon, NaN, object (array like list and keyword like dict), undefined
+
+// s= "hello"
+// s1= 'hai'
+// s2= `hai`
+
+// console.log(typeof s2)
+
+// keyword - let - localStorage, var - globalThis, const - local 
+
+// let x = 10
+// {
+//     let x = 5
+//     console.log("inner:",x);
+// }
+// console.log("outside:", x);
+// out 
+// inner: 5
+// outside: 10
+
+
+
+// var x = 10
+// {
+//     var x = 5
+//     console.log("inner:",x);
+// }
+// console.log("outside:", x);
+// out 
+// inner: 5
+// outside: 5
+
+
+
+// const pi = 3.14
+// {
+//     const pi = 5
+//     console.log("inner:",pi);
+// }
+// console.log("outside:", pi);
+// out 
+// inner: 5
+// outside: 3.14
+
+
+
+// let cant redefine the variable 
+
+
+
+// let y    can assign later
+// y = 4
+
+// var x    can assign later
+// x =  5
+
+// const pi = 3.14 need to assing when initialise
+
+
+// number 
+
+// x = 10
+// console.log(typeof x);
+// out 
+// mumber
+
+
+// x = 10.5
+// console.log(typeof x);
+// out 
+// number
+
+
+
+// booleon 
+
+
+// x = true   //small letter 
+// console.log(typeof x);
+// out 
+// booleon 
+
+// let x
+// console.log(typeof x);
+// out 
+// undefined
+
+// a=5;b=7
+
+
+// NaN 
+
+
+// console.log("hello"/2);
+// out 
+// NaN
+
+
+// console.log("2"+2); 22
+// console.log(2+2+"4"); 44
+
+
+// object - keyword and array
+
+
+// arr1 = [1,2,33,4,2] array
+// console.log(arr1);
+// console.log(typeof arr1); object
+
+
+// obj = {                    
+//     name:"shuahib",
+//     place:"kkd"
+// }
+// console.log(obj);
+// console.log(typeof obj);    object
+
+
+
+// Operators 
+
+// arithmetic - +, -, *, /, **, %, ++, --
+
+// let x = 10
+// console.log(x++); post increment - first return the x then increment
+// out 
+// 10
+
+// let y = 20
+// console.log(++y); pre increment - first uncrement then return 
+// out 
+// 21
+
+
+
+// x = 10
+// y = x++
+// console.log(x); 11
+// console.log(y); 10
+
+
+// x = 10
+// y = ++x
+// console.log(x); 11
+// console.log(y); 11
+
+
+
